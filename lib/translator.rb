9ea330919b7ce6ff683b838fcdbff2e12cd601ad
Jason Herrera <baytownjason@yahoo.com>
2020-12-21 20:19:57 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,53 @@
 # require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
+
+def load_library(file_path)
   # code goes here
+  inside_yaml_file = YAML.load_file(file_path)
+
+  final_hash = inside_yaml_file.each_with_object({}) do |(key, values), hash|
+  
+    hash[key] = {}
+  
+    hash[key][:english] = values.shift
+    hash[key][:japanese] = values.shift
+  end
+  final_hash
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(hash, eng_symbol)
   # code goes here
+  #hash.each do |word, eng_syms|
+   # if hash[word][eng_syms][0] == eng_symbol
+    #  puts hash[word][eng_syms][1]
+   # end
+  #end
 end
 
-def get_english_meaning
+def get_english_meaning(file_path, jap_symbol)
   # code goes here
+  inside_yaml_file = YAML.load_file(file_path)
+  
+  final_hash = inside_yaml_file.each_with_object({}) do |(key, values), hash|
+  
+    hash[key] = {}
+  
+    hash[key][:english] = values.shift
+    hash[key][:japanese] = values.shift
+  end
+  japanese_loop = final_hash.each do |sym_name, languages|
+    languages.each do |language, symbol|
+      #binding.pry
+      if symbol == jap_symbol
+        return sym_name
+      end
+    end
+  end
+  japanese_loop
 end
+  
+
+#load_library("./lib/emoticons.yml")
+#get_english_meaning("./lib/emoticons.yml", 'o_O')
